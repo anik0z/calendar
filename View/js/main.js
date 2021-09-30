d1c515ce33650945527bb509c4490447d4a70582
anik0z <rafaelgonzalezalbes@gmail.com>
2021-09-30 13:31:51 +0200

    adding Js for when you double click in one day open the add view

--- View/js/main.js
+++ View/js/main.js
@@ -1,6 +1,6 @@
 (function() {
     const listDays = document.getElementsByClassName('eventAdd');
-    
+
     for(let i = 0;i < listDays.length;i++){
         listDays[i].addEventListener('dblclick', function (e) {
             // activate the form
